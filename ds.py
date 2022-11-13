a6db2e7be0e795729af19c4fd3291b8c52bc7fce
Philippe Heitzmann <phil062497@gmail.com>
2022-11-12 23:53:30 -0500

    adding GeneralPreprocessing and other classes

---
+++ ds.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+class DateTransforms():
+    
+    def __init__(self):
+        pass 
+    
+    def get_period_groupby(self, df, period = 'M', date_col = 'date') -> pd.DataFrame:
+        '''Inputs: period (str) one of \'M\',\'D\',\'Y\' '''
+        allowed_periods = ['D','M','Y']
+        if period not in allowed_periods:
+            raise ValueError(f'Unrecognized period value {period}. Period should be one of {allowed_periods}')
+        if period == 'Y':
+            output_col = 'year'
+        if period == 'M':
+            output_col = 'month_year'
+        if period == 'D':
+            output_col = 'full_year'
+        df[output_col] = df[date_col].dt.to_period('M')
+        return df
+    
+    
+    def get_days_of_interest(self, df, period_col = 'month_year', date_col = 'date', agg = 'max'):
+        '''Get days specified by agg for periods of interest'''
+        days = df.groupby(period_col)[[date_col]].agg({date_col:agg}).reset_index(drop=True)
+        days = days.merge(df, how = 'inner', on = date_col)
+        return days
+    
+    
+    def get_date_mask(self, df, start_date, end_date, date_col = 'date'):
+        return (df[date_col] > start_date) & (df[date_col] <= end_date)
+    
+    
+class NumericalTransforms():
+    
+    def __init__(self):
+        pass 
+    
+    def get_pct_change(self, series: pd.Series):
+        return series.pct_change().fillna(0)
+    
+    def is_min(self, series, date_val):
+        earliest_day = series.min()
+        return earliest_day == date_val
+
+    def is_max(self, series, date_val):
+        earliest_day = series.max()
+        return earliest_day == date_val
+    
+
+class GeneralPreprocessing():
+
+    def remove_missing(self, df: pd.DataFrame, col = 'value', missing_pattern = '.') -> pd.DataFrame:
+        return df.loc[df[col] != missing_pattern]
+
+
+
+
